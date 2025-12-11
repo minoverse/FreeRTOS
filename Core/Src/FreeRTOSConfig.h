1c2f1dd2cce46ddc0c80a739cb87a2a5689396fb
minoverse <121744450+minoverse@users.noreply.github.com>
2025-12-11 11:13:19 +0100

    Add FreeRTOS configuration header file

---
+++ Core/Src/FreeRTOSConfig.h
@@ -0,0 +1,60 @@
+#ifndef FREERTOS_CONFIG_H
+#define FREERTOS_CONFIG_H
+
+#include "stm32f4xx_hal.h"
+
+/*-----------------------------------------------------------
+ * Application specific definitions.
+ *----------------------------------------------------------*/
+
+#define configUSE_PREEMPTION                    1
+#define configUSE_TIME_SLICING                  1
+#define configUSE_IDLE_HOOK                     1
+#define configUSE_TICK_HOOK                     0
+
+#define configCPU_CLOCK_HZ                      (SystemCoreClock)
+#define configTICK_RATE_HZ                      ((TickType_t)1000)
+
+#define configMAX_PRIORITIES                    7
+#define configMINIMAL_STACK_SIZE                ((uint16_t)128)
+#define configTOTAL_HEAP_SIZE                   ((size_t)(20 * 1024))
+#define configMAX_TASK_NAME_LEN                 16
+#define configUSE_16_BIT_TICKS                  0
+#define configIDLE_SHOULD_YIELD                 1
+
+/* Mutexes, Semaphores, EventGroups, Timers */
+#define configUSE_MUTEXES                       1
+#define configUSE_RECURSIVE_MUTEXES             1
+#define configUSE_COUNTING_SEMAPHORES           1
+#define configUSE_QUEUE_SETS                    1
+#define configUSE_TIMERS                        1
+#define configTIMER_TASK_PRIORITY               (configMAX_PRIORITIES - 1)
+#define configTIMER_QUEUE_LENGTH                10
+#define configTIMER_TASK_STACK_DEPTH            256
+#define configUSE_TASK_NOTIFICATIONS            1
+#define configTASK_NOTIFICATION_ARRAY_ENTRIES   4
+#define configUSE_EVENT_GROUPS                  1
+
+/* Stack / Heap checks */
+#define configCHECK_FOR_STACK_OVERFLOW          2
+#define configUSE_MALLOC_FAILED_HOOK            1
+#define configUSE_TRACE_FACILITY                1
+#define configUSE_STATS_FORMATTING_FUNCTIONS    1
+
+/* Interrupt priority configuration (Cortex-M) */
+#define configKERNEL_INTERRUPT_PRIORITY         0xF0
+#define configMAX_SYSCALL_INTERRUPT_PRIORITY    0xA0
+#define configLIBRARY_LOWEST_INTERRUPT_PRIORITY 15
+#define configLIBRARY_MAX_SYSCALL_INTERRUPT_PRIORITY 5
+
+/* Map the FreeRTOS port interrupt handlers to their CMSIS names. */
+#define vPortSVCHandler      SVC_Handler
+#define xPortPendSVHandler   PendSV_Handler
+#define xPortSysTickHandler  SysTick_Handler
+
+/* Hook function prototypes */
+void vApplicationIdleHook(void);
+void vApplicationStackOverflowHook(TaskHandle_t xTask, char *pcTaskName);
+void vApplicationMallocFailedHook(void);
+
+#endif /* FREERTOS_CONFIG_H */
