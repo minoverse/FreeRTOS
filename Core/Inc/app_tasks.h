74b1bd3d41ed432e59f6ff9a0206c51af63c0853
minoverse <121744450+minoverse@users.noreply.github.com>
2025-12-11 11:05:22 +0100

    Expand task and callback function declarations
    Added new task functions and timer callback functions to app_tasks.h.

--- Core/Inc/app_tasks.h
+++ Core/Inc/app_tasks.h
@@ -1,21 +1,43 @@
 #ifndef __APP_TASKS_H
 #define __APP_TASKS_H
 
-#include "FreeRTOS.h"
-#include "task.h"
-#include "queue.h"
+#ifdef __cplusplus
+extern "C" {
+#endif
 
-void Task_UART_RX(void *argument);
-void Task_UART_TX(void *argument);
-void Task_CommandProcessor(void *argument);
-void Task_SensorLogger(void *argument);
-void Task_Monitor(void *argument);
+#include "app_rtos.h"
 
-/* Demo helpers */
+/* ===== Task functions ===== */
+void Task_UART_RX(void *pvParameters);
+void Task_UART_TX(void *pvParameters);
+void Task_CommandProcessor(void *pvParameters);
+void Task_SensorLogger(void *pvParameters);
+void Task_Monitor(void *pvParameters);
+void Task_LowPriority(void *pvParameters);
+void Task_MediumPriority(void *pvParameters);
+void Task_HighPriority(void *pvParameters);
+void Task_RecursiveUser(void *pvParameters);
+void Task_Button(void *pvParameters);
+void Task_BitNotify(void *pvParameters);
+
+/* ===== Timer callback functions ===== */
+void vLedTimerCallback(TimerHandle_t xTimer);
+void vHeartbeatTimerCallback(TimerHandle_t xTimer);
+
+/* ===== Helper functions ===== */
 void Debug_Transmit(const char *msg);
 void ProcessCommand(const char *cmd);
 
-void Recursive_Test_Level1(void);
-void Recursive_Test_Level2(void);
+/* Recursive mutex demo helpers */
+void Recursive_Level1(void);
+void Recursive_Level2(void);
 
+/* Critical section demo */
+void CriticalNested_Level1(void);
+void CriticalNested_Level2(void);
+
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* __APP_TASKS_H */
